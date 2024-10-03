cdaaef703e5e3075a21480f69d1a4bcabaebc4d3
Mariyan Tomov <murcho123@gmail.com>
2024-10-03 17:56:52 +0300

    new version

--- Categories/Models/Category.cs
+++ Categories/Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace Categories.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Categories.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int DisplayOrder { get; set; }
     }
 }
